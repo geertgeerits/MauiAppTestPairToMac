ea73257bf41cf2b0d1d01cf60bd87d33d784035f
Geert Geerits <ggeerits@hotmail.com>
2022-11-27 09:53:45 +0100

    Add project files.

---
+++ MauiAppTestPairToMac/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppTestPairToMac;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiAppTestPairToMac/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppTestPairToMac;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
